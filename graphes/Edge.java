baf7f9f5177da0d42464f661da102ce85e7b9873
AlexGinestra <alex.ginestra4@etu.univ-lorraine.fr>
2018-12-26 00:13:21 +0100

    reindentation code

---
+++ graphes/Edge.java
@@ -0,0 +1,25 @@
+package graphes;
+
+class Edge
+{
+   int from;
+   int to;
+   boolean used;
+   
+    public Edge(int x, int y)
+    {
+		this.from = x;
+		this.to = y;
+		this.used = false;
+    }
+    
+    final int other(int v)
+    {
+    		if (this.from == v) {
+    			return this.to;
+    		}
+    		else {
+    			return this.from;
+    		}
+    }    
+}
