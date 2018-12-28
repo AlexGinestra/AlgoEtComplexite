4a97c75cb3e535211101cd86e15493a05c5582da
AlexGinestra <alex.ginestra4@etu.univ-lorraine.fr>
2018-12-28 03:07:39 +0100

    doc .pages + creation kruskal.java

---
+++ graphes/Kruskal.java
@@ -0,0 +1,18 @@
+package graphes;
+
+public class Kruskal {
+	
+	
+	/*
+	 * Q2 : implantation de l'algorithme de Kruskal
+	 *  param: graph G
+	 *  return: covering tree of G
+	 */
+	public static Graph algorithme(Graph g) {
+		
+		// TODO
+		return null;
+		
+	}
+
+}
