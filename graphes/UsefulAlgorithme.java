19e4484f2ee7d43594a88b4fc06f53f9dd40d60f
AlexGinestra <alex.ginestra4@etu.univ-lorraine.fr>
2018-12-29 02:27:37 +0100

    detection cycle OKé

--- graphes/UsefulAlgorithme.java
+++ graphes/UsefulAlgorithme.java
@@ -11,7 +11,7 @@ public class UsefulAlgorithme {
 		boolean hasCycle = false;
 		if(!(g.adj(e.from).size() == 0 || g.adj(e.to).size() == 0)) {
 			for(Edge eTemp : g.adj(e.to)) {
-				hasCycle = UsefulAlgorithme.cycleDetectionRecursive(g, eTemp, e.to, 0);
+				hasCycle = UsefulAlgorithme.cycleDetectionRecursive(g, eTemp, e.from, 0);
 				if(hasCycle) {
 					return true;
 				}
@@ -23,9 +23,18 @@ public class UsefulAlgorithme {
 	
 	/*
 	 * used by cycleDetection()
+	 * param :
+	 * - g is the graph without the new edge
+	 * - e is the current edge
+	 * - dest is the vertice where the new edge start
+	 * - depthcount is used to know if there is a other cycle
+	 * 
+	 * return :
+	 * - true if there is a cycle in the graphe with or without the new edge
+	 * - false in other case
 	 */
 	@SuppressWarnings("unchecked")
-	public static boolean cycleDetectionRecursive(Graph g, Edge e, int dest, int depthCount) {
+	private static boolean cycleDetectionRecursive(Graph g, Edge e, int dest, int depthCount) {
 		boolean hasCycle = false;
 		if(e.to == dest || e.from == dest || depthCount > g.getEdgesNumber()) {
 			return true;
@@ -39,5 +48,22 @@ public class UsefulAlgorithme {
 			}
 		}
 		return hasCycle;
+	}
+	
+	
+	public static void main(String... args) {
+		Graph g = new Graph(5);
+		g.addEdge(new Edge(0, 1));
+		g.addEdge(new Edge(1, 2));
+		g.addEdge(new Edge(3, 4));
+
+		System.out.println(UsefulAlgorithme.cycleDetection(g, new Edge(3, 4)));
+		System.out.println(UsefulAlgorithme.cycleDetection(g, new Edge(2, 3)));
+		System.out.println(UsefulAlgorithme.cycleDetection(g, new Edge(0, 4)));
+		System.out.println(UsefulAlgorithme.cycleDetection(g, new Edge(2, 0)));
+
+
+		
+		
 	}
 }
