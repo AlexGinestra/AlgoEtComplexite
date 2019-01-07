74baa521a79997e223efa017ec2dd2d57b217b3e
AlexGinestra <alex.ginestra4@etu.univ-lorraine.fr>
2019-01-07 23:17:23 +0100

    q13

--- partie2/AlgoCouleur.java
+++ partie2/AlgoCouleur.java
@@ -3,24 +3,44 @@ package partie2;
 public class AlgoCouleur {
 
 	public static int combinaison(int k, int n, int b, int m) {
+		int res = 0;
 		
-		if(n == 0 || b == n) {
-			
-			return 0;
-			
-		}else if(n == (b+m)) {
-			
-			return 1+(k*(combinaison(n-1,n-1,b-1,m)+combinaison(n-1,n-1,b,m-1)));
-			
-		}else {
-			
-			return 1+((k-b-m)*(combinaison(k-b-m,n-1,b-1,m)+combinaison(k-b-m,n-1,b,m-1)));
+		if( b+m == n ) {
+			if(n == 0 || b == n) {
+				return 1;
+			}
+			if(b > 0 && m > 0) {
+				res += 1*combinaison(k-1, n-1, b-1, m) + (n-1)*combinaison(k-1,n-1,b,m-1);
+			}
+			else if(b > 0) {
+				res += 1*combinaison(k-1, n-1, b-1, m);
+			}
+			else if(m > 0) {
+				res += n*combinaison(k-1,n-1,b,m-1);
+			}
 			
 		}
+		else {
+			if(n == 0 || b == n) {
+				return 1;
+			}
+			if(b > 0 && m > 0) {
+				res += 1*combinaison(k-1, n-1, b-1, m) + (n-1)*combinaison(k-1,n-1,b,m-1);
+			}
+			else if(b > 0) {
+				res += 1*combinaison(k-1, n-1, b-1, m);
+			}
+			else if(m > 0) {
+				res += n*combinaison(k-1,n-1,b,m-1);
+			}
+			res += (k-n)*combinaison(k-1,n-1,b,m);
+		}
+		
+		return res;
 	}
 
 	public static void main(String... args) {
-		//System.out.println("resultat:"+AlgoCouleur.combinaison(6,4,1,2));
+		System.out.println("resultat:"+AlgoCouleur.combinaison(6,4,1,2));
 	}
 	
 }
